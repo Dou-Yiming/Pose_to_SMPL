6cdea719a6b94cd7ef5de1b5c8c7894827b31e66
Iridoudou <2534936416@qq.com>
2021-08-09 12:58:04 +0800

    update folder

--- fit/output/HumanAct12/tmp.py
+++
@@ -1,8 +0,0 @@
-import pickle
-import numpy as np
-a=pickle.load(open("./P01G01R01F0001T0064A0101_params.pkl",'rb'))
-for key,value in a.items():
-    if key =='label':
-        print("{}: {}".format(key,value))
-    else:
-        print("{}: {}".format(key,np.array(value).shape))
