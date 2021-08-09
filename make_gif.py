93538f468229c8a98194a4d9a95715b0e25ee825
Iridoudou <2534936416@qq.com>
2021-08-09 12:53:35 +0800

    update gif

--- make_gif.py
+++ make_gif.py
@@ -4,4 +4,4 @@ images = []
 filenames = sorted(fn for fn in os.listdir('./fit/output/HumanAct12/picture/fit/P01G01R01F0001T0064A0101') )
 for filename in filenames:
     images.append(imageio.imread('./fit/output/HumanAct12/picture/fit/P01G01R01F0001T0064A0101/'+filename))
-imageio.mimsave('./assets/fit.gif', images, duration=0.3)
+imageio.mimsave('./assets/fit.gif', images, duration=0.25)
